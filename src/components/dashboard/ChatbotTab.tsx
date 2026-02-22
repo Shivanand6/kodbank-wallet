794fdf48c98fb1da2d0f0bb3d1fca8b3fc20208d
Shivanand6 <shivanandshirur99@gmail.com>
2026-02-22 21:56:18 +0530

    Add ChatbotTab component with integration notice

---
+++ src/components/dashboard/ChatbotTab.tsx
@@ -0,0 +1,10 @@
+const ChatbotTab = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Chatbot</h2>
+      <p>Old chatbot removed. New chatbot integrated separately.</p>
+    </div>
+  );
+};
+
+export default ChatbotTab;
